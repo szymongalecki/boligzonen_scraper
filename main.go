122a0210f19cca80a2f151f11207102e05d0169e
Szymon Gałecki <szymongalecki@gmail.com>
2023-04-07 23:16:43 +0200

    concurrently scrape entire page of apartments

--- main.go
+++ main.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"os"
 	"strconv"
 	"strings"
 
@@ -11,13 +9,77 @@ import (
 )
 
 type Apartment struct {
-	Rooms int
-	Area  int
-	Rent  int
+	Ref       int
+	Rooms     int
+	Area      int
+	Rent      int
+	Latitude  float64
+	Longitude float64
 }
 
-func main() {
-	apartment := Apartment{}
+func apartment(url string) (apartment Apartment) {
+	c := colly.NewCollector()
+
+	c.OnRequest(func(r *colly.Request) {
+		r.Headers.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36")
+		fmt.Println("Visiting", r.URL)
+	})
+
+	c.OnError(func(r *colly.Response, err error) {
+		fmt.Println("Error", err.Error())
+	})
+
+	c.OnHTML(".reference-number", func(h *colly.HTMLElement) {
+		value := h.Text
+		value = strings.Trim(value, "Sagsnummer: ")
+		ref, _ := strconv.Atoi(value)
+		apartment.Ref = ref
+	})
+
+	c.OnHTML(".section-bar", func(h *colly.HTMLElement) {
+		selection := h.DOM
+		label := selection.Find(".section-bar-label").Text()
+		value := selection.Find(".section-bar-value").Text()
+
+		switch label {
+		case "Antal værelser":
+			rooms, _ := strconv.Atoi(value)
+			apartment.Rooms = rooms
+		case "Størrelse":
+			value = strings.Trim(value, "m2")
+			value = strings.TrimSpace(value)
+			area, _ := strconv.Atoi(value)
+			apartment.Area = area
+		case "Husleje":
+			value = strings.Trim(value, ",-")
+			value = strings.ReplaceAll(value, ".", "")
+			rent, _ := strconv.Atoi(value)
+			apartment.Rent = rent
+		}
+	})
+
+	c.OnHTML("div[data-lat][data-lng]", func(h *colly.HTMLElement) {
+		lat := h.Attr("data-lat")
+		lng := h.Attr("data-lng")
+		latitude, _ := strconv.ParseFloat(lat, 64)
+		longitude, _ := strconv.ParseFloat(lng, 64)
+		apartment.Latitude = latitude
+		apartment.Longitude = longitude
+
+	})
+
+	// c.OnScraped(func(r *colly.Response) {
+	// 	enc := json.NewEncoder(os.Stdout)
+	// 	enc.SetIndent("", " ")
+	// 	enc.Encode(apartment)
+	// })
+
+	c.Visit(url)
+
+	return apartment
+}
+
+func links(url string) (links []string) {
 	c := colly.NewCollector()
 
 	c.OnRequest(func(r *colly.Request) {
@@ -33,37 +95,29 @@ func main() {
 		fmt.Println("Response Code", r.StatusCode)
 	})
 
-	c.OnHTML(".section-bar", func(h *colly.HTMLElement) {
-		selection := h.DOM
-
-		label := selection.Find(".section-bar-label").Text()
-		value := selection.Find(".section-bar-value").Text()
-
-		switch label {
-		case "Antal værelser":
-			rooms, _ := strconv.Atoi(value)
-			// fmt.Println("Rooms", rooms)
-			apartment.Rooms = rooms
-		case "Størrelse":
-			value = strings.Trim(value, "m2")
-			value = strings.TrimSpace(value)
-			area, _ := strconv.Atoi(value)
-			// fmt.Println("Area", area)
-			apartment.Area = area
-		case "Husleje":
-			value = strings.Trim(value, ",-")
-			value = strings.ReplaceAll(value, ".", "")
-			rent, _ := strconv.Atoi(value)
-			// fmt.Println("Rent", rent)
-			apartment.Rent = rent
-		}
+	c.OnHTML(".property-partial[href]", func(h *colly.HTMLElement) {
+		rel := h.Attr("href")
+		root := "https://boligzonen.dk/"
+		link := root + rel
+		links = append(links, link)
 	})
 
-	c.OnScraped(func(r *colly.Response) {
-		enc := json.NewEncoder(os.Stdout)
-		enc.SetIndent("", " ")
-		enc.Encode(apartment)
-	})
+	c.Visit(url)
 
-	c.Visit("https://boligzonen.dk/lejeboliger/3-vaerelses-lejlighed-i-kastrup-10")
+	return links
+}
+
+func main() {
+	links := links("https://boligzonen.dk/lejebolig/kobenhavn-kommune")
+	apartments := make(chan Apartment)
+
+	for _, link := range links {
+		go func(link string, apartments chan Apartment) {
+			apartments <- apartment(link)
+		}(link, apartments)
+	}
+
+	for i := 0; i < 18; i++ {
+		fmt.Println(<-apartments)
+	}
 }
